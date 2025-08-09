1da2aed3e4ba9e34e0ac6fc3b020a7a3ba730e00
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-09 17:52:38 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review006_AllVariables.java
@@ -0,0 +1,15 @@
+public class Review006_AllVariables {
+    public static void main(String[] args){
+        int age = 24;
+        float pie = 3.14f;
+        char letter = 'L';
+        boolean choice = true;
+        String message = "Hello!";
+
+        System.out.println(age);
+        System.out.println(pie);
+        System.out.println(letter);
+        System.out.println(choice);
+        System.out.println(message);
+    }
+}
