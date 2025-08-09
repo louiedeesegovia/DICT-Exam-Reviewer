db0d002d7918ede8e6ac5bb256bbb55ec78fbed9
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-10 07:14:36 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review007_StringAdding.java
@@ -0,0 +1,8 @@
+public class Review007_StringAdding {
+    public static void main(String[] args){
+        String firstName = "Louie ";
+        String secondName = "Dee ";
+        String lastName = "Segovia";
+        System.out.println(firstName+secondName+lastName);
+    }
+}
