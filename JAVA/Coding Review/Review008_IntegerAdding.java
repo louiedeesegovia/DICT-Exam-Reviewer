810d86f1e551a470f87b603e757f2829ba94890d
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-10 07:15:57 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review008_IntegerAdding.java
@@ -0,0 +1,8 @@
+public class Review008_IntegerAdding {
+    public static void main(String[] args){
+        int x = 1;
+        int y = 4;
+        int z = 3;
+        System.out.println(x+y+z);
+    }
+}
