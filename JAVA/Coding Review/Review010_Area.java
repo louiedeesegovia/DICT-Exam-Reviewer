9d52effc4a42d9a4fc6037c602b22377bdbb71b9
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-10 07:17:33 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review010_Area.java
@@ -0,0 +1,13 @@
+public class Review010_Area {
+    public static void main(String[] args){
+        float length = 23f;
+        float width = 45.6f;
+        float area;
+
+        area = length * width;
+
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area is: " + area);
+    }
+}
