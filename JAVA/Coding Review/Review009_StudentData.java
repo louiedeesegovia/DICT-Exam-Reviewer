334dc7248f5689802ff859550a0ac0a43ff064c2
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-10 07:16:33 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review009_StudentData.java
@@ -0,0 +1,26 @@
+public class Review009_StudentData {
+    public static void main(String[] args){
+        String studentFirstName = "Louie ";
+        String studentSecondName = "Dee ";
+        String studentMiddleName = "Reyes ";
+        String studentLastName = "Segovia ";
+
+        int studentID = 1001;
+
+        int studentAge = 24;
+
+        float studentCollegeFee = 1234.56f;
+
+        boolean studentEnrolled = true;
+
+        char studentSection = 'A';
+
+        System.out.println("Student's Name: " + studentFirstName+studentSecondName+studentMiddleName+studentLastName);
+        System.out.println("Student's ID: " + studentID);
+        System.out.println("Student's Age: " + studentAge);
+        System.out.println("Student College Fee: " + studentCollegeFee);
+        System.out.println("Student Enrolled: " + studentEnrolled);
+        System.out.println("Section: " + studentSection);
+
+    }
+}
