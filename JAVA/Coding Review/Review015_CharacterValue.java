3eabea9f31904176a31793d04528155acf4fa5c3
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-11 16:13:46 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review015_CharacterValue.java
@@ -0,0 +1,9 @@
+public class Review015_CharacterValue {
+    public static void main(String[] args){
+        char myVar1 = 67, myVar2 = 68, myVar3 = 69;
+
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+    }
+}
