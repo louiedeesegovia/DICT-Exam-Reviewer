6c650e52b4a387ee44623b4ada4b92bf75a9c16b
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-11 16:11:58 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review012_ScientificNumber.java
@@ -0,0 +1,9 @@
+public class Review012_ScientificNumber {
+    public static void main(String[] args){
+        float f1 = 35e3f;
+        double d2 = 12E4d;
+
+        System.out.println(f1);
+        System.out.println(d2);
+    }
+}
