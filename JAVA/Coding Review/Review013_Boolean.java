c178e813f2f3ca451150d736239240dda8d935ac
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-11 16:12:43 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review013_Boolean.java
@@ -0,0 +1,9 @@
+public class Review013_Boolean {
+    public static void main(String[] args){
+        boolean x = false;
+        boolean y = true;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
