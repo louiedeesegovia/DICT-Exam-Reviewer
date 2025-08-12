a0fa5f786d0f4a4cf0088dedda28b6730d22c678
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-12 21:46:26 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review018_CastingGrade.java
@@ -0,0 +1,12 @@
+public class Review018_CastingGrade {
+    public static void main(String[] args) {
+        int perfectScore = 500;
+
+        int yourScore = 423;
+
+        float percentage = (float) yourScore / perfectScore * 100.0f;
+
+        System.out.println("Final Grade Percentage: " + percentage);
+
+    }
+}
