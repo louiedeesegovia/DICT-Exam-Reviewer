7e876a92d71216d8634e08e41cc56f58fa2ba057
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-13 09:23:54 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review024_StringLength.java
@@ -0,0 +1,7 @@
+public class Review024_StringLength {
+    public static void main(String[] args){
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        System.out.println("String length: "+txt.length());
+    }
+}
