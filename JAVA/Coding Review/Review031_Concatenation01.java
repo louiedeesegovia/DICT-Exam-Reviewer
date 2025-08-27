eae46b2b52eef5d252667b06e4e08d299103b0ed
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-27 19:35:19 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review031_Concatenation01.java
@@ -0,0 +1,8 @@
+public class Review031_Concatenation01 {
+    public static void main(String[] args){
+        String fname = "Louie";
+        String lname = "Segovia";
+
+        System.out.println(fname + " " + lname);
+    }
+}
