0917a361b52aad913b50100f6c7bf33353986b0a
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-29 18:12:40 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review035_SpecialCharacter.java
@@ -0,0 +1,7 @@
+public class Review035_SpecialCharacter {
+    public static void main(String[] args){
+        String Boxer = "Manny \"PacMan\" Pacquaio";
+
+        System.out.print(Boxer);
+    }
+}
