4e7d48543a02380f98a07b12b6d42b8bf6ef786a
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-29 18:12:19 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review034_Concatenation04.java
@@ -0,0 +1,9 @@
+public class Review034_Concatenation04 {
+    public static void main(String[] args){
+        int x = 20;
+        String y = "25";
+        String z = x + y;
+
+        System.out.println("Year: " + z);
+    }
+}
