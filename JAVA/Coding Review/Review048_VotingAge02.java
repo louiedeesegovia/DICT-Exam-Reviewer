d2ed55e99cfaf94ca0f51d6d84237d9ab18c7379
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-03 07:37:25 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review048_VotingAge02.java
@@ -0,0 +1,12 @@
+public class Review048_VotingAge02 {
+    public static void main(String[] args){
+        int myAge = 17;
+        int votingAge = 18;
+
+        if(myAge >= votingAge){
+            System.out.println("Old enough to vote!");
+        } else{
+          System.out.println("Not old enough to vote.");
+        }
+    }
+}
