9c71e70fb98b76ed139169259def403dfc20da7e
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-03 07:36:24 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review047_VotingAge01.java
@@ -0,0 +1,8 @@
+public class Review047_VotingAge01 {
+    public static void main(String[] args){
+        int myAge = 25;
+        int votingAge = 18;
+
+        System.out.println(myAge >= votingAge);
+    }
+}
