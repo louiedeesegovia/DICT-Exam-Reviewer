ff43a99534e411108ce71c76c00f4bea9e623ca6
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-02 21:40:15 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review041_MathRandom.java
@@ -0,0 +1,6 @@
+public class Review041_MathRandom {
+    public static void main(String[] args){
+        int randomNum = (int)(Math.random()*7); // 101
+        System.out.println(randomNum);
+    }
+}
