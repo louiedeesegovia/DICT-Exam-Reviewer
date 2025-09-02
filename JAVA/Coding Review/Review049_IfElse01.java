c4a0138228ca50f60d2e86da8abbec21d54c7b16
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-03 07:37:55 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review049_IfElse01.java
@@ -0,0 +1,9 @@
+public class Review049_IfElse01 {
+    public static void main(String[] args){
+        boolean isRaining = true;
+
+        if(isRaining){
+            System.out.println("Bring an umbrella!");
+        }
+    }
+}
