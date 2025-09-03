3ab7fd3e3889420df02a0aa03789ef1e9e013433
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-03 21:40:56 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review051_IfElse03.java
@@ -0,0 +1,12 @@
+public class Review051_IfElse03 {
+    public static void main(String[] args){
+        boolean isRaining = false;
+
+        if(isRaining){
+            System.out.println("Bring an umbrella");
+        } else{
+            System.out.println("No rain today, no need for an umbrella!");
+        }
+
+    }
+}
