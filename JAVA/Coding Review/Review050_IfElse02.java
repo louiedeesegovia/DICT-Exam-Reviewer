cb8ce1781528014efc3f050dfe105ae31d280c8f
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-03 21:40:37 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review050_IfElse02.java
@@ -0,0 +1,9 @@
+public class Review050_IfElse02 {
+    public static void main(String[] args){
+        boolean isLightOn = true;
+        if (isLightOn == true){
+            System.out.println("The Light is on.");
+        }
+        System.out.println("This line always runs.");
+    }
+}
