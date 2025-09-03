1def94743ef7e78ba42c916ac8fab7475ca22a81
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-03 21:41:15 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review052_IfElse04.java
@@ -0,0 +1,13 @@
+public class Review052_IfElse04 {
+    public static void main(String[] args){
+       int weather = 2;
+
+        if(weather == 1) {
+            System.out.println("It's sunny");
+        } else if(weather == 2){
+            System.out.println("It's hot!");
+        }else{
+            System.out.println("Bring umbrella!");
+        }
+    }
+}
