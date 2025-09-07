42b4a4b89a20faec8dbfed7d44cd20ddea6c30e1
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-07 16:35:12 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review054_ShortHandIfElse.java
@@ -0,0 +1,8 @@
+public class Review054_ShortHandIfElse {
+    public static void main(String[] args){
+        int time = 20;
+
+        String result = (time < 18) ? "Good morning" : "Good Evening!";
+        System.out.println(result);
+    }
+}
