79cb06793905f054c8cc390d810d34bb034cedeb
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-07 16:35:41 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review055_NestedIfElse.java
@@ -0,0 +1,14 @@
+public class Review055_NestedIfElse {
+    public static void main(String[] args){
+        int x = 15;
+        int y = 31;
+
+        if(x < 16){
+            System.out.println("x is less than 16");
+
+            if(y > 30){
+                System.out.println("And y is greater than 31");
+            }
+        }
+    }
+}
