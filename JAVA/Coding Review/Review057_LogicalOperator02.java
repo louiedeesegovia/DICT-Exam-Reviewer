1647e013b9354f08ed9154520539008703b492e4
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-07 16:37:03 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review057_LogicalOperator02.java
@@ -0,0 +1,11 @@
+public class Review057_LogicalOperator02 {
+    public static void main(String[] args){
+        int a = 300;
+        int b = 30;
+        int c = 3000;
+
+        if(a > b || a > c){
+            System.out.println("Atlest one condition is true!");
+        }
+    }
+}
