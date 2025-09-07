13b2f96ca0a36cb3fcd4d8afbc3c02bd3738effb
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-07 16:48:07 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review058_LogicalOperator03.java
@@ -0,0 +1,12 @@
+public class Review058_LogicalOperator03 {
+    public static void main(String[] args){
+        int a = 4;
+        int b = 40;
+
+        if(!(a < b)){
+            System.out.println("a is less than b");
+        } else{
+            System.out.println("It is in reversed!");
+        }
+    }
+}
