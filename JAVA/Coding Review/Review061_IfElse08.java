577c63f31d9d2d3e4f879d6c695e2a8ad130563f
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-12 20:58:16 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review061_IfElse08.java
@@ -0,0 +1,13 @@
+public class Review061_IfElse08 {
+    public static void main(String[] args){
+        int MyNum = 10;
+
+        if(MyNum > 0){
+            System.out.println("The number is positive.");
+        } else if(MyNum < 0){
+            System.out.println("The number is negative.");
+        } else{
+            System.out.println("The number is equal to zero.");
+        }
+    }
+}
