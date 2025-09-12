9136e4c682a8a31930e0d7fb5cb79c8145d22cd7
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-12 20:59:57 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review064_Switch01.java
@@ -0,0 +1,29 @@
+public class Review064_Switch01 {
+    public static void main(String[] args){
+        int day = 4;
+
+        switch (day){
+            case 1:
+                System.out.println("It's Sunday!");
+                break;
+            case 2:
+                System.out.println("It's Monday!");
+                break;
+            case 3:
+                System.out.println("It's Tuesday!");
+                break;
+            case 4:
+                System.out.println("It's Wednesday!");
+                break;
+            case 5:
+                System.out.println("It's Thursday!");
+                break;
+            case 6:
+                System.out.println("It's Friday!");
+                break;
+            case 7:
+                System.out.println("It's Saturday!");
+                break;
+        }
+    }
+}
