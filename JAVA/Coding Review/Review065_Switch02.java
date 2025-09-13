719cd71b63b77920ff4a67f7c7b9a9af5c2de665
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-13 18:43:42 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review065_Switch02.java
@@ -0,0 +1,20 @@
+public class Review065_Switch02 {
+    public static void main(String[] args){
+        int day = 4;
+
+        switch (day){
+            case 1:
+                System.out.println("It's Sunday!");
+                break;
+            case 7:
+                System.out.println("It's Sabbath!");
+                break;
+            default:
+                System.out.println("It's weekdays");
+                break;
+
+        }
+
+
+    }
+}
