2ae0909ab6804446fd90c9e5531abf8bc4af0989
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-13 18:45:49 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review069_DoWhile02.java
@@ -0,0 +1,10 @@
+public class Review069_DoWhile02 {
+    public static void main(String[] args) {
+        int i = 10;
+
+        do {
+            System.out.println("i is "+ i + " ");
+            i++;
+        } while (i < 5);
+    }
+}
