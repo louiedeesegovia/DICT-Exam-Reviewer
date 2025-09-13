9eefe1e6373fa7295f18fe11fd32ef183c8a2917
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-13 18:44:28 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review067_WhileLoop02.java
@@ -0,0 +1,13 @@
+public class Review067_WhileLoop02 {
+    public static void main(String[] args){
+        int countDown = 10;
+
+        while(countDown > 0){
+            System.out.print("[" + countDown + "] ");
+            countDown--;
+        }
+
+        System.out.println("Happy New Year!");
+    }
+
+}
