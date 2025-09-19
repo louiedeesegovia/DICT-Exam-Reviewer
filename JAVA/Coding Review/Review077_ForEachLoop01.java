d67f87129847ff2a44d647eefffaea9b7b529056
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-19 21:21:27 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review077_ForEachLoop01.java
@@ -0,0 +1,9 @@
+public class Review077_ForEachLoop01 {
+    public static void main(String[] args){
+        String[] cellphones = {"Nokia", "Samsung", "Iphone", "Blackberry", "Oppo"};
+
+        for(String cell : cellphones){
+            System.out.print(cell + " ");
+        }
+    }
+}
