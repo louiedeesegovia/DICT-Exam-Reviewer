8cf2b25278f54e1cd42f21232f31091da3739166
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-20 19:01:15 +0800

    Add files via upload

---
+++ JAVA/Coding Review/Review083_ForLoop09.java
@@ -0,0 +1,12 @@
+public class Review083_ForLoop09 {
+    public static void main(String[] args){
+        //Outer loop
+        for(int i = 1; i <= 2; i++){
+            System.out.println("Outer: " + i);
+
+            for(int j = 1; j <= 3; j++){
+                System.out.println("Inner: " + j);
+            }
+        }
+    }
+}
